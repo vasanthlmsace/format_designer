a7257921ff30eee85879d4eac8d63ccc3beb7e74
vasanthlmsace <vasanthr.lmsace@gmail.com>
2021-11-24 16:44:35 +0530

    initial source updated

---
+++ classes/output/renderer.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace format_designer\output;
+
+use core_courseformat\output\section_renderer;
+
+/**
+ * Basic renderer for designer format.
+ *
+ * @package   format_designer
+ * @copyright 2021 bdecent gmbh <https://bdecent.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class renderer extends section_renderer {
+
+    /**
+     * Renders the content widget.
+     * @param renderable $widget instance with renderable interface
+     * @return string the widget HTML
+     */
+    public function render_content($widget) {
+        $data = $widget->export_for_template($this);
+        return $this->render_from_template('format_designer/courseformat/content',
+        $widget->export_for_template($this));
+    }
+
+    /**
+     * Generate the section title, wraps it in a link to the section page if page is to be displayed on a separate page.
+     *
+     * @param section_info|stdClass $section The course_section entry from DB
+     * @param stdClass $course The course entry from DB
+     * @return string HTML to output.
+     */
+    public function section_title($section, $course) {
+        return $this->render(course_get_format($course)->inplace_editable_render_section_name($section));
+    }
+
+    /**
+     * Generate the section title to be displayed on the section page, without a link.
+     *
+     * @param section_info|stdClass $section The course_section entry from DB
+     * @param int|stdClass $course The course entry from DB
+     * @return string HTML to output.
+     */
+    public function section_title_without_link($section, $course) {
+        return $this->render(course_get_format($course)->inplace_editable_render_section_name($section, false));
+    }
+}
---
+++ lang/en/format_designer.php
@@ -0,0 +1,108 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component Designer course format.
+ *
+ * @package   format_designer
+ * @copyright 2021 bdecent gmbh <https://bdecent.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addsections'] = 'Add designer sections';
+$string['currentsection'] = 'This designer section';
+$string['dateformat'] = 'Date format';
+$string['dateformat_help'] = 'Date format for completion info (e.g. Complete by x date)';
+$string['deletesection'] = 'Delete designer section';
+$string['designer:changesectionoptions'] = 'Change section options, such as type.';
+$string['editsection'] = 'Edit designer section';
+$string['editsectionname'] = 'Edit designer section name';
+$string['hidefromothers'] = 'Hide designer section';
+$string['newsectionname'] = 'New name for designer section {$a}';
+$string['page-course-view-designer'] = 'Any course main page in Designer format';
+$string['page-course-view-designer-x'] = 'Any course page in Designer format';
+$string['pluginname'] = 'Designer format';
+$string['privacy:metadata'] = 'The Designer format plugin does not store any personal data.';
+$string['sectionname'] = 'Designer section';
+$string['section0name'] = 'General';
+$string['sectiontype'] = 'Section type';
+$string['showfromothers'] = 'Show designer section';
+$string['completiontrackingmanual'] = 'Manual completion';
+$string['completiontrackingautomatic'] = 'Automatic completion';
+$string['completeby'] = 'Complete by';
+$string['usstandarddate'] = '%m-%d-%Y %I:%M %p';
+$string['monthandday'] = '%b %e';
+$string['timeagoyear'] = 'year';
+$string['timeagomonth'] = 'month';
+$string['timeagoweek'] = 'week';
+$string['timeagoday'] = 'day';
+$string['timeagohour'] = 'hour';
+$string['timeagominute'] = 'minute';
+$string['timeagosecond'] = 'second';
+$string['timeago'] = 'ago';
+$string['timeagojustnow'] = 'just now';
+$string['overdue'] = 'Overdue';
+$string['duetoday'] = 'Due today';
+$string['completed'] = 'Completed on';
+$string['passed'] = 'Passed on';
+$string['failed'] = 'Failed on';
+$string['start'] = 'Start';
+$string['calltoactionview'] = 'View <i class="fa fa-chevron-circle-right"></i>';
+$string['calltoactioncontinue'] = 'Continue <i class="fa fa-chevron-circle-right"></i>';
+$string['calltoactionstart'] = 'Start <i class="fa fa-chevron-circle-right"></i>';
+$string['calltoactionrestricted'] = 'Restricted <i class="fa fa-lock"></i>';
+
+$string['completion-alt-auto-enabled'] = 'The system marks this item complete according to conditions';
+$string['completion-alt-auto-fail'] = 'Completed (did not achieve pass grade)';
+$string['completion-alt-auto-n'] = 'Not completed';
+$string['completion-alt-auto-n-override'] = 'Not completed (set by {$a->overrideuser})';
+$string['completion-alt-auto-pass'] = 'Completed (achieved pass grade)';
+$string['completion-alt-auto-y'] = 'Completed';
+$string['completion-alt-auto-y-override'] = 'Completed (set by {$a->overrideuser})';
+$string['completion-alt-manual-enabled'] = 'Students can manually mark this item complete';
+$string['completion-alt-manual-n'] = 'Not completed. Select to mark as complete.';
+$string['completion-alt-manual-n-override'] = 'Not completed (set by {$a->overrideuser}). Select to mark as complete.';
+$string['completion-alt-manual-y'] = 'Completed. Select to mark as not complete.';
+$string['completion-alt-manual-y-override'] = 'Completed (set by {$a->overrideuser}). Select to mark as not complete.';
+$string['completion-fail'] = 'Completed (did not achieve pass grade)';
+$string['completion-n'] = 'Not completed';
+$string['completion-n-override'] = 'Not completed (set by {$a->overrideuser})';
+$string['completion-pass'] = 'Completed (achieved pass grade)';
+$string['completion-y'] = 'Completed';
+$string['completion-y-override'] = 'Completed (set by {$a->overrideuser})';
+
+$string['completion-shorthand-alt-auto-enabled'] = 'The system marks this item complete according to conditions';
+$string['completion-shorthand-alt-auto-fail'] = 'Failed <i class="fa fa-exclamation-triangle text-white"></i>';
+$string['completion-shorthand-alt-auto-n'] = 'Not completed';
+$string['completion-shorthand-alt-auto-n-override'] = 'Not completed <i class="fa fa-exclamation-triangle text-white"></i>';
+$string['completion-shorthand-alt-auto-pass'] = 'Completed (achieved pass grade)';
+$string['completion-shorthand-alt-auto-y'] = 'Completed';
+$string['completion-shorthand-alt-auto-y-override'] = 'Completed <i class="fa fa-exclamation-triangle text-white"></i>';
+$string['completion-shorthand-alt-manual-enabled'] = 'Students can manually mark this item complete';
+$string['completion-shorthand-alt-manual-n'] = 'Not completed';
+$string['completion-shorthand-alt-manual-n-override'] = 'Not completed';
+$string['completion-shorthand-alt-manual-y'] = 'Completed';
+$string['completion-shorthand-alt-manual-y-override'] = 'Completed <i class="fa fa-exclamation-triangle text-white"></i>';
+$string['completion-shorthand-fail'] = 'Completed <i class="fa fa-exclamation-triangle text-white"></i>';
+$string['completion-shorthand-n'] = 'Not completed';
+$string['completion-shorthand-n-override'] = 'Not completed <i class="fa fa-exclamation-triangle text-white"></i>';
+$string['completion-shorthand-pass'] = 'Completed (achieved pass grade)';
+$string['completion-shorthand-y'] = 'Completed';
+$string['completion-shorthand-y-override'] = 'Completed <i class="fa fa-exclamation-triangle text-white"></i>';
+$string['modvisit'] = 'Visited {$a} times';
+$string['link'] = "Text links (Default)";
+$string['list'] = "List";
+$string['cards'] = "Cards";
