f00366770ebd34e579c20f66b94ed554defc28bb
vasanthlmsace <vasanthr.lmsace@gmail.com>
2026-02-04 19:59:13 +0530

    Update the source.

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -1507,6 +1507,25 @@ class renderer extends \core_courseformat\output\section_renderer {
      * @param course_format $format the course format
      * @return String the course index HTML.
      */
+    public function course_index_drawer(course_format $format): ?string {
+        if ($format->uses_course_index()) {
+            include_course_editor($format);
+            return $this->render_from_template('format_designer/courseformat/courseindex/drawer', []);
+        }
+        return '';
+    }
+
+    /**
+     * Get the course index drawer with placeholder.
+     *
+     * The default course index is loaded after the page is ready. Format plugins can override
+     * this method to provide an alternative course index.
+     *
+     * If the format is not compatible with the course index, this method will return an empty string.
+     *
+     * @param course_format $format the course format
+     * @return String the course index HTML.
+     */
 
     /**
      * Generate the classes for the activity elements visibility classes.
