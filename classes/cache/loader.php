d813ad71e1bb8164491ea40fa3d761c7f7ca8b03
vasanthlmsace <vasanthr.lmsace@gmail.com>
2026-02-23 17:43:40 +0530

    Update the source.

--- classes/cache/loader.php
+++ classes/cache/loader.php
@@ -30,7 +30,7 @@ global $CFG;
 
 require_once(__DIR__ . '/loader_trait.php');
 
-if (!class_exists('\core_cache\application_cache')) {
+if (!class_exists('\core_cache\application_cache') && (file_exists( $CFG->dirroot . '/cache/classes/loaders.php'))) {
     require_once($CFG->dirroot . '/cache/classes/loaders.php');
     /**
      * Custom cache loader to handle the smart menus and items deletion for older Moodle versions.
